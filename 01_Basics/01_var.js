6543c66a48fa3c740ca4e3f615e9c127384253e2
mdjabir17 <mahmadjabirkhorajiya@gmail.com>
2025-02-06 16:46:22 +0000

    Variables

---
+++ 01_Basics/01_var.js
@@ -0,0 +1,8 @@
+const accountId= 123456;
+let accountName = "Jabir"; 
+var accountPassword = "1718";
+accountCity = "Rajkot";
+
+let accountState;
+
+console.table([accountId, accountName, accountPassword, accountCity, accountState]);
