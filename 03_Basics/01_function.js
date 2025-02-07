e9677ef0ec31a4579b6156982d5150c78f59465d
mdjabir17 <mahmadjabirkhorajiya@gmail.com>
2025-02-07 16:46:52 +0000

    basic of function

---
+++ 03_Basics/01_function.js
@@ -0,0 +1,12 @@
+function abc(num1, num2) {
+    return num1 + num2
+}
+
+const x = abc(10, 7)
+console.log(x);
+
+function user(username){
+    return`${username} just logged in`
+}
+
+console.log(user("abc"));
