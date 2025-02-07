659a9b7854e9fcabd8a967ef26cf1a3a3e134de2
mdjabir17 <mahmadjabirkhorajiya@gmail.com>
2025-02-07 17:59:13 +0000

    some practise for condition statements

---
+++ apna/03_prac.js
@@ -0,0 +1,15 @@
+let size = "M";
+
+if (size === "XL") {
+    console.log("Price for XL size Popcorn is: 250" );
+}
+else if(size === "L"){
+    console.log("Price for L size Popcorn is: 200" );
+}
+else if(size === "M"){
+    console.log("Price for M size Popcorn is: 100" );
+}
+
+else{
+    console.log("Price for Small size Popcorn is: 50" );
+}
