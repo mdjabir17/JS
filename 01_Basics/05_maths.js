dae5a972a3d7728b82cb49e04904ce5eb4c0d3b4
mdjabir17 <mahmadjabirkhorajiya@gmail.com>
2025-02-07 06:04:09 +0000

    Math function

---
+++ 01_Basics/05_maths.js
@@ -0,0 +1,24 @@
+console.log(Math);
+
+console.log(Math.abs(-17));
+
+console.log(Math.round(17.50));
+
+console.log(Math.ceil(17.1));//Round to Upper
+
+console.log(Math.floor(17.1));//Round to Lower
+
+console.log(Math.sqrt(49));
+
+console.log(Math.random()*100);
+console.log(Math.round(Math.random()*100));
+
+console.log(Math.min(1,2,3,4,5,6,7,8,9,10));
+console.log(Math.max(1,2,3,4,5,6,7,8,9,10));
+
+
+const min = 10;
+const max = 20;
+
+
+console.log(Math.floor(Math.random() * (max - min +1) +min));
