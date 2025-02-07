2e5efb6e0ab4e24cc7f9122c65ab8f355feb8c6e
mdjabir17 <mahmadjabirkhorajiya@gmail.com>
2025-02-07 08:19:05 +0000

    more on arrays

--- 02_Basics/01_arrays.js
+++ 02_Basics/01_arrays.js
@@ -6,11 +6,10 @@ console.log(myArr.toString());
 
 console.log(myArr.sort());
 
-console.log(myArr.slice(0, 5));
+console.log(myArr.slice(0, 5));// simply slice means taking value copy from array and main array remains as it is....
 console.log(myArr);
 
-
-console.log(myArr.splice(0, 3));
+console.log(myArr.splice(0, 3));// simply splice means taking value from array and cut the value from main array....
 console.log(myArr);
 
 
