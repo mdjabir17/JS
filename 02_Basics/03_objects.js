5b0872c6e4a67c94844e8946d758f0efc91ab1b1
mdjabir17 <mahmadjabirkhorajiya@gmail.com>
2025-02-07 11:04:32 +0000

    objects in js

---
+++ 02_Basics/03_objects.js
@@ -0,0 +1,25 @@
+
+const mySym = Symbol("key1");
+
+const personalDetails = {
+    name : "Jabir",
+    [mySym] : "mykey1",
+    age : 21,
+    email : "jabirkhorajiya@gmail.com",
+    isLoggedIn : false,
+    lastLoginDays : ["Monday", "Saturday"]
+}
+
+console.log(personalDetails);
+console.log(personalDetails["name"]);
+console.log(personalDetails["age"]);
+console.log(personalDetails["email"]);
+console.log(personalDetails[mySym]);
+
+
+Object.freeze(personalDetails);
+personalDetails.age = 28;  // can't change because of freeze statement
+personalDetails.email = "jabirkhorajiya@google.com" //can't change because of freeze statement
+console.log(personalDetails);
+
+
