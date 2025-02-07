e297ee432183b1bed9445d8b69d1d65322b4524a
mdjabir17 <mahmadjabirkhorajiya@gmail.com>
2025-02-07 17:35:18 +0000

    if statement in JS

---
+++ apna/01_if_condition.js
@@ -0,0 +1,17 @@
+const fname = "Jabir";
+if (fname == "Jabir") {
+    console.log(`My name is ${fname}`);
+} 
+
+let color = "green";
+
+if (color === "red") {
+    console.log("Stop!");
+}
+if (color === "yellow") {
+    console.log("Slow Down!");
+}
+if (color === "green") {
+    console.log("Go!");
+}
+
