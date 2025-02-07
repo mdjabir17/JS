e59289a046bdf6cad962d9dab5008e2719f22127
mdjabir17 <mahmadjabirkhorajiya@gmail.com>
2025-02-07 18:02:51 +0000

    Switch Statement in js

---
+++ apna/04_switch.js
@@ -0,0 +1,16 @@
+let color = "green";
+
+switch (color) {
+    case "red":
+        console.log("Color is Red");
+        break;
+    case "green":
+        console.log("Color is Green");
+        break;
+    case "yellow":
+        console.log("Color is Yellow");
+        break;
+    default:
+        console.log("Broken");
+        
+}
