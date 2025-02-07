3b9d93f1e4c9c609961639f965eb644ec75c4595
mdjabir17 <mahmadjabirkhorajiya@gmail.com>
2025-02-07 05:35:53 +0000

    numbers in js

---
+++ 01_Basics/04_numbers.js
@@ -0,0 +1,14 @@
+const num = 400;
+console.log(num);
+
+const balance = new Number(17.181718);
+console.log(balance);
+
+console.log(balance.toString());
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const hundreds = 1000000;
+
+console.log(hundreds.toLocaleString());
+console.log(hundreds.toLocaleString('en-IN'));
