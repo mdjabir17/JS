4d91bdbf4f7ab82673e65bdf7664ba436ef5a55b
mdjabir17 <mahmadjabirkhorajiya@gmail.com>
2025-02-07 08:16:52 +0000

    more on arrays

---
+++ 02_Basics/01_arrays.js
@@ -0,0 +1,16 @@
+const myArr = [0,1,2,5,4,3]
+
+console.log(myArr);
+
+console.log(myArr.toString());
+
+console.log(myArr.sort());
+
+console.log(myArr.slice(0, 5));
+console.log(myArr);
+
+
+console.log(myArr.splice(0, 3));
+console.log(myArr);
+
+
