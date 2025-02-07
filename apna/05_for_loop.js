77ac85adc6bb7668bd31c58dd9c0c0bcc8c1055d
mdjabir17 <mahmadjabirkhorajiya@gmail.com>
2025-02-07 18:30:36 +0000

    for loop in js

---
+++ apna/05_for_loop.js
@@ -0,0 +1,23 @@
+for ( let a = 1; a <= 10; a++) {
+    console.log(a);
+    
+}
+
+for ( let i = 2; i <= 10; i = i+2) {
+    
+    console.log(i);
+    
+}
+
+console.log("Print ODD Numbers");   
+for (let j = 1; j <= 15; j = j+2){
+    console.log(j);
+}
+
+console.log("Print Even Numbers");   
+    
+for ( let m = 2; m <= 20; m = m+2){
+    console.log(m);
+    
+}
+
