2ad36d40a8aa86403bf93169117e3ef988bc6a52
mdjabir17 <mahmadjabirkhorajiya@gmail.com>
2025-02-07 05:21:47 +0000

    More on Strings

---
+++ 01_Basics/03_str.js
@@ -0,0 +1,30 @@
+const name = "Jabir I Khorajiya";
+const age = 21;
+
+console.log(`Hello Everyone my name is ${name} and i am ${21} years old`);
+
+
+const myName = new String('Khorajiya Jabir');
+console.log(myName);
+
+
+console.log(myName.toUpperCase());
+ 
+console.log(myName.length);
+console.log(myName.indexOf('K'));
+console.log(myName.charAt(8));
+
+
+const nameOne = "    jabir   "
+
+console.log(nameOne);
+console.log(nameOne.trim());
+
+const url = "https//khorajiya.com/jabir%20khorajiya"
+console.log(url);
+
+console.log(url.replace('%20','_'));
+
+
+console.log(myName.split(' '));
+console.log(name.split(' '));
