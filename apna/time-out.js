8630d3ce0785d2f217a0d17e29a187cd3a503b39
mdjabir17 <mahmadjabirkhorajiya@gmail.com>
2025-02-09 18:32:03 +0530

    Time-Out function

---
+++ apna/time-out.js
@@ -0,0 +1,21 @@
+// console.log("Hello Everyone");
+// setTimeout ( () => {
+//   console.log("ABC");
+  
+// }, 4000);
+
+
+// console.log("I am");
+
+
+
+console.log("Hii There!");
+
+setTimeout ( () => {
+  console.log("My Stream...");
+  
+}, 2000
+
+);
+
+console.log("Welcome to...");
